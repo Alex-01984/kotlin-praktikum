30d56d1914dc2542e3323757381606a239080f38
Alex-01984 <Rubik10965@yandex.ru>
2026-04-24 21:29:28 +0300

    KS-3-2: fix errors in concatenation task

--- Concatenation.kt
+++ Concatenation.kt
@@ -1,11 +1,11 @@
 fun main (){
-var last_Name = "Андреева"
-val first_Name = "Татьяна"
-val middle_Name = "Сергеевна"
-var age = 20
+ var lastName = "Андреева"
+  val firstName = "Татьяна"
+  val middleName = "Сергеевна"
+  var age = 20
 
-println("$last_Name $first_Name $middle_Name $age")
-last_Name = "Сидорова"
-age = 22
-println("$last_Name $first_Name $middle_Name $age")
+  println("$firstName $lastName $middleName, $age")
+  lastName = "Сидорова"
+  age = 22
+  println("$firstName $lastName $middleName, $age")
 }
