121b473646eb2b76eb1dea752fd70dc8d01ef6ae
Alex-01984 <Rubik10965@yandex.ru>
2026-04-24 20:48:25 +0300

    KS-3-2: concatenation task solution

---
+++ Concatenation.kt
@@ -0,0 +1,11 @@
+fun main (){
+var last_Name = "Андреева"
+val first_Name = "Татьяна"
+val middle_Name = "Сергеевна"
+var age = 20
+
+println("$last_Name $first_Name $middle_Name $age")
+last_Name = "Сидорова"
+age = 22
+println("$last_Name $first_Name $middle_Name $age")
+}
