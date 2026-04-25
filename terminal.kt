f65640b0679a2c4634aa1eccf4ba7ebeb4baf078
Alex-01984 <Rubik10965@yandex.ru>
2026-04-26 00:02:25 +0300

    KS-4-2: terminal

--- terminal.kt
+++ terminal.kt
@@ -1,17 +1,14 @@
-// Предварительно заданные параметры категории "Average"
-const val WEIGHT_MIN = 35 // не включительно
-const val WEIGHT_MAX = 100 // включительно
-const val VOLUME_MAX = 100 // не включительно
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME_MAX = 100
 
 fun main() {
-    // 1-й груз: 20 кг / 80 л
     val weight1 = 20
     val volume1 = 80
     println(
             "Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': ${weight1 > WEIGHT_MIN && weight1 <= WEIGHT_MAX && volume1 < VOLUME_MAX}"
     )
 
-    // 2-й груз: 50 кг / 100 л
     val weight2 = 50
     val volume2 = 100
     println(
