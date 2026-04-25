224523a5b653cba80e49f9c79f56d8c125263fc4
Alex-01984 <Rubik10965@yandex.ru>
2026-04-25 15:52:08 +0300

    KS-3-4: сhess

---
+++ chess.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var whereFrom = "E2"
+    var whereTo = "E4"
+    var number = 1
+    var move = "$whereFrom-$whereTo;$number"
+    println(move)
+    whereFrom = "D2"
+    whereTo = "D3"
+    number++
+    move = "$whereFrom-$whereTo;$number"
+    println(move)
+}
