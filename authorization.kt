495a34434c3b3d2e9bde0b25bf07bfec74d615f7
Alex-01984 <Rubik10965@yandex.ru>
2026-04-27 19:04:34 +0300

    KS-5-1: authorization

--- authorization.kt
+++ authorization.kt
@@ -1,15 +1,13 @@
 fun main() {
-    // Генерируем числа для примера (можно вынести в константы при необходимости)
-    val number1 = 10
-    val number2 = 13
-    val correctAnswer = number1 + number2
+    // Генерируем случайные числа для капчи в диапазоне 0..10
+    val num1 = (0..10).random()
+    val num2 = (0..10).random()
 
-    // Выводим задание и считываем ответ
-    print("Сложите два числа: $number1 + $number2 = ")
-    val userInput = readln().toIntOrNull()
+    // Полноценное сообщение с понятной инструкцией
+    print("Подтвердите, что вы не робот, сложите два числа: $num1 + $num2 = ")
 
-    // Проверяем ответ
-    if (userInput == correctAnswer) {
+    // Считываем ответ и сразу сравниваем с суммой прямо в условии
+    if (readln().toIntOrNull() == num1 + num2) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещён")
