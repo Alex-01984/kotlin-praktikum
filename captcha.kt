e38293de70765d1b4513f21fed05843332c1ad2c
Alex-01984 <Rubik10965@yandex.ru>
2026-04-29 19:29:17 +0300

    KS-6-5: captcha

--- captcha.kt
+++ captcha.kt
@@ -1,14 +1,16 @@
 fun main() {
-    for (attempt in 1..3) {
+    var attempt = 1
+
+    do {
         val num1 = (1..9).random()
         val num2 = (1..9).random()
 
         print("Попытка $attempt. Подтвердите, что вы не робот: $num1 + $num2 = ")
-
-        if (readln().toIntOrNull() == num1 + num2) {
+        if (readln().toInt() == num1 + num2) {
             println("Добро пожаловать!")
             return
         }
-    }
+        attempt++
+    } while (attempt <= 3)
     println("Доступ запрещён")
 }
