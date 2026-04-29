140630e327420b50a20401470146d6f8033b3933
Alex-01984 <Rubik10965@yandex.ru>
2026-04-29 22:38:41 +0300

    KS-7-1: password

--- password.kt
+++ password.kt
@@ -1,10 +1,10 @@
-/*Создай программу, которая генерирует шестизначные пароли. Пусть пароль будет состоять из чередующихся цифр и строчных букв латинского алфавита (без специальных символов). Например, k5p6m2. Выведи сгенерированный пароль в консоль. */
 import kotlin.random.Random
 
 fun main() {
     val letters = "abcdefghijklmnopqrstuvwxyz"
     val digits = "0123456789"
     var password = ""
+
     for (i in 1..6) {
         if (i % 2 == 1) {
             // Нечётные позиции (1, 3, 5) → буква
@@ -14,5 +14,6 @@ fun main() {
             password += digits[Random.nextInt(digits.length)]
         }
     }
+
     println(password)
 }
