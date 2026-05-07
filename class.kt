c658c8eb1c5b3195449b3a29c8c38b2aab0952cc
Alex-01984 <Rubik10965@yandex.ru>
2026-05-07 22:59:29 +0300

    KS-11-1: class

---
+++ class.kt
@@ -0,0 +1,20 @@
+// Класс вынесен за пределы main() и использует первичный конструктор
+class User(
+        val id: Int,
+        var login: String,
+        var password: String,
+        var email: String,
+)
+
+fun main() {
+    //  Данные передаются сразу при создании объектов
+    val person1 = User(1, "admin", "123", "person1@yandex.ru")
+    val person2 = User(2, "user", "456", "person2@yandex.ru")
+
+    println(
+            "ID: ${person1.id}, Логин: ${person1.login}, Пароль: ${person1.password}, Почта: ${person1.email}"
+    )
+    println(
+            "ID: ${person2.id}, Логин: ${person2.login}, Пароль: ${person2.password}, Почта: ${person2.email}"
+    )
+}
