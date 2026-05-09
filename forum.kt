5671a61c91abc3c0ba041f968b56eab5924d13b7
Alex-01984 <Rubik10965@yandex.ru>
2026-05-09 19:05:35 +0300

    KS-11-5: forum

--- forum.kt
+++ forum.kt
@@ -4,34 +4,21 @@ class ForumMessage(val authorId: Int, val message: String)
 
 class Forum {
     private var id = 0
-
-    // списки для хранения данных (приватные)
-    // список объектов ForumMember
     private val listForum = mutableListOf<ForumMember>()
-
-    // список объектов ForumMessage
     private val forumMessages = mutableListOf<ForumMessage>()
 
-    // Метод createNewUser(name: String): ForumMember
     fun createNewUser(name: String): ForumMember {
-        // генерирует ID внутри
         id++
-        // создаёт объект (скрыто)
         val newUser = ForumMember(id, name)
-        // добавляет в список
         listForum.add(newUser)
-        // возвращает готового пользователя
         return newUser
     }
 
-    // Метод createNewMessage
     fun createNewMessage(
         authorId: Int,
         text: String,
     ): ForumMessage? {
-        // проверяет, есть ли пользователь с таким ID
         if (listForum.any { it.userId == authorId }) {
-            // если есть — создаёт и сохраняет сообщение
             val newMessage = ForumMessage(authorId, text)
             forumMessages.add(newMessage)
             return newMessage
@@ -39,7 +26,6 @@ class Forum {
         return null
     }
 
-    // печатает все сообщения добавленные на форум
     fun printThread() {
         for (message in forumMessages) {
             val author = listForum.find { message.authorId == it.userId }
