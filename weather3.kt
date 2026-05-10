1fc57a53a46904b00bc471bba92c1bf95d35440c
Alex-01984 <Rubik10965@yandex.ru>
2026-05-10 16:54:42 +0300

    KS-12-3: weather3

--- weather3.kt
+++ weather3.kt
@@ -1,20 +1,16 @@
+const val KELVIN_TO_CELSIUS_OFFSET = 273
+
 class Weather3(dayK: Int, nightK: Int, hasPrecipitation: Boolean) {
-    // Поля объявлены и проинициализированы в ТЕЛЕ класса
-    var dayTemperature: Int = dayK - 273
-    var nightTemperature: Int = nightK - 273
-    var precipitation: Boolean = hasPrecipitation
+    val dayTemperature: Int = dayK - KELVIN_TO_CELSIUS_OFFSET
+    val nightTemperature: Int = nightK - KELVIN_TO_CELSIUS_OFFSET
+    val precipitation: Boolean = hasPrecipitation
 
     fun printWeather() {
-        println(
-            "Дневная: $dayTemperature°C, Ночная: $nightTemperature°C, Осадки: $precipitation",
-        )
+        println("Дневная: $dayTemperature°C, Ночная: $nightTemperature°C, Осадки: $precipitation")
     }
 }
 
 fun main() {
-    // Данные передаём в конструктор (в Кельвинах)
-    // 293K ≈ 20°C, 283K ≈ 10°C
     val day1 = Weather3(293, 283, true)
-
     day1.printWeather()
 }
