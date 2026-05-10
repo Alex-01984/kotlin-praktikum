dc62ba3273fc555616137be7b3dbc6e2b8bf0015
Alex-01984 <Rubik10965@yandex.ru>
2026-05-10 22:33:34 +0300

    KS-12-5: weather5

--- weather5.kt
+++ weather5.kt
@@ -1,13 +1,19 @@
+import kotlin.random.Random
+
 const val KELVIN_CELSIUS = 273
 
 class Weather5(val dayK: Int, val nightK: Int, val hasPrecipitation: Boolean)
 
 fun main() {
-    val weatherList = mutableListOf<Weather5>()
-    repeat(30) {
-        val day = Weather5((270..295).random(), (265..285).random(), listOf(true, false).random())
-        weatherList.add(day)
-    }
+    val weatherList =
+        List(30) {
+            Weather5(
+                dayK = (270..295).random(),
+                nightK = (265..285).random(),
+                hasPrecipitation = Random.nextBoolean(),
+            )
+        }
+
     val dayTemps = weatherList.map { it.dayK - KELVIN_CELSIUS }
     val nightTemps = weatherList.map { it.nightK - KELVIN_CELSIUS }
     val rainyDays = weatherList.count { it.hasPrecipitation }
