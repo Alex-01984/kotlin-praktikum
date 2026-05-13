8549e32ca9d2a39a97da0ec8d3f0ea1d660a6ab9
Alex-01984 <Rubik10965@yandex.ru>
2026-05-13 19:15:44 +0300

    KS-13-5: Contact5

---
+++ Contact5.kt
@@ -0,0 +1,36 @@
+class Contact5(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+)
+
+fun main() {
+    val personList = mutableListOf<Contact5>()
+
+    while (true) {
+        print("Введите имя: ")
+        val nameInput = readln()
+        if (nameInput.lowercase() == "exit") break
+
+        print("Введите номер телефона: ")
+        val phoneInput = readln()
+        val phoneNumber: Long
+
+        try {
+            phoneNumber = phoneInput.toLong()
+        } catch (e: NumberFormatException) {
+            println("Ошибка: ${e::class.simpleName}")
+            continue
+        }
+
+        print("Введите название компании: ")
+        val companyInput = readlnOrNull()
+
+        personList.add(Contact5(nameInput, phoneNumber, companyInput))
+    }
+
+    println("\nСписок контактов:")
+    personList.forEach {
+        println("Имя: ${it.name}\nНомер: ${it.phoneNumber}\nКомпания: ${it.company ?: "<не указано>"}")
+    }
+}
