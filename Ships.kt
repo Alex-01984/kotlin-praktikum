d6e91e7eb82d37ee239b87dec6f1e7c269ec471c
Alex-01984 <Rubik10965@yandex.ru>
2026-05-14 00:41:05 +0300

    KS-14-1: Contact5

--- Ships.kt
+++ Ships.kt
@@ -1,20 +1,20 @@
-open class Liner(
-    open val speed: Int = 60,
-    open val passengerCapacity: Int = 200,
-    open val cargoCapacity: Int = 250,
-)
+open class Liner {
+    open val speed: Int = 60
+    open val passengerCapacity: Int = 200
+    open val cargoCapacity: Int = 250
+}
 
-class Cargo(
-    override val speed: Int = 40,
-    override val passengerCapacity: Int = 40,
-    override val cargoCapacity: Int = 600,
-) : Liner(speed = speed, passengerCapacity = passengerCapacity, cargoCapacity = cargoCapacity)
+class Cargo : Liner() {
+    override val speed: Int = 40
+    override val passengerCapacity: Int = 40
+    override val cargoCapacity: Int = 600
+}
 
-class Icebreaker(
-    override val speed: Int = 25,
-    override val passengerCapacity: Int = 35,
-    val canBreakIce: Boolean = true,
-) : Liner(speed = speed, passengerCapacity = passengerCapacity)
+class Icebreaker : Liner() {
+    override val speed: Int = 25
+    override val passengerCapacity: Int = 35
+    val canBreakIce: Boolean = true
+}
 
 fun main() {
     val liner = Liner()
