e66b44d92e1309d785f6195bf30d3670f771ef45
Alex-01984 <Rubik10965@yandex.ru>
2026-06-13 12:42:58 +0300

    KS-15-3: edits

--- Forum.kt
+++ Forum.kt
@@ -1,20 +1,12 @@
 abstract class ForumUser(val name: String) {
-    abstract fun readForum()
+    fun readForum() = println("$name читает сообщения на форуме.")
 
-    abstract fun writeMessage()
+    fun writeMessage() = println("$name пишет новое сообщение.")
 }
 
-class RegularUser(name: String) : ForumUser(name) {
-    override fun readForum() = println("$name читает сообщения на форуме.")
-
-    override fun writeMessage() = println("$name пишет новое сообщение.")
-}
+class RegularUser(name: String) : ForumUser(name)
 
 class Admin(name: String) : ForumUser(name) {
-    override fun readForum() = println("$name (администратор) читает сообщения на форуме.")
-
-    override fun writeMessage() = println("$name (администратор) пишет новое сообщение.")
-
     fun deleteMessage() = println("$name удалил(а) сообщение.")
 
     fun deleteUser() = println("$name заблокировал(а) и удалил(а) пользователя.")
