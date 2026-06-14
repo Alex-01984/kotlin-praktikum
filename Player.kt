b0d1f923afe255b5e21e549d0faae91396070a07
Alex-01984 <Rubik10965@yandex.ru>
2026-06-14 22:09:03 +0300

    ks-16-5: edits

--- Player.kt
+++ Player.kt
@@ -1,8 +1,13 @@
 class Player(val name: String, private var health: Int, private var attackPower: Int) {
     fun takeDamage(amount: Int) {
+        if (health <= 0) {
+            println("$name уже мертв и не может получать урон.")
+            return
+        }
         health -= amount
         if (health <= 0) {
             die()
+            return
         }
         println("$name получил $amount урона. Текущее здоровье: $health")
     }
@@ -29,6 +34,7 @@ fun main() {
     hero.heal(10)
     hero.takeDamage(20)
     hero.heal(5)
-    hero.takeDamage(70)
-    hero.heal(10)
+    hero.takeDamage(70) // Этот удар должен убить
+    hero.heal(10) // Эта попытка лечения должна быть заблокирована
+    hero.takeDamage(10) // Эта попытка получения урона тоже должна быть заблокирована
 }
